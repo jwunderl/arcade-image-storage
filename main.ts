cbc093e5c51015872f9eac757e7907672c858598
Joey Wunderlich <jwunderl@users.noreply.github.com>
2020-10-27 17:34:44 -0700

    just save the width and height in the buffer

--- main.ts
+++ main.ts
@@ -8,20 +8,20 @@ namespace imagesettings {
     export function writeImage(key: string, toSave: Image) {
         const w = toSave.width;
         const h = toSave.height;
-        const imgBuf = pins.createBuffer(h * w);
+        const imgBuf = pins.createBuffer(2 + h * w);
         const rowBuf = pins.createBuffer(h);
+        imgBuf.setUint8(0, w);
+        imgBuf.setUint8(1, h);
         for (let i = 0; i < h; ++i) {
             // load one image at a time...
             toSave.getRows(i, rowBuf);
             // and save to the buffer we're going to store, offset from the start.
-            imgBuf.write(i * w, rowBuf);
+            imgBuf.write(2 + i * w, rowBuf);
         }
 
         const namespacedSetting = settingKey(key);
 
         settings.writeBuffer(namespacedSetting, imgBuf);
-        settings.writeNumber(`${namespacedSetting}--w`, w);
-        settings.writeNumber(`${namespacedSetting}--h`, h);
     }
 
     function settingKey(key: string) {
@@ -36,12 +36,12 @@ namespace imagesettings {
         const loadedImg = settings.readBuffer(namespacedSetting);
         if (!loadedImg)
             return undefined;
-        const w = settings.readNumber(`${namespacedSetting}--w`);
-        const h = settings.readNumber(`${namespacedSetting}--h`);
+        const w = loadedImg.getUint8(0);
+        const h = loadedImg.getUint8(1);
 
         const outputImg = image.create(w, h); 
         for (let i = 0; i < h; ++i) {
-            const row = loadedImg.slice(i * w, w);
+            const row = loadedImg.slice(2 + i * w, w);
             outputImg.setRows(i, row);
         }
 
